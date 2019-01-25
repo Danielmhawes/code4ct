ed04e98b36a45c629dff8b43adb953de569b96ab
Danielmhawes <danielmhawes002@gmail.com>
2019-01-25 16:23:54 +0200

    added a counter section with javascript

--- js/main.js
+++ js/main.js
@@ -14,3 +14,62 @@ $(document).ready(function(){
       $slides.css('animation-duration', slidesLength*2+'s');
     }
   }
+
+  // count
+  var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter-value').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
+});
+
+  // $(document).ready(function() {
+
+  //   var counters = $(".count");
+  //   var countersQuantity = counters.length;
+  //   var counter = [];
+  
+  //   for (i = 0; i < countersQuantity; i++) {
+  //     counter[i] = parseInt(counters[i].innerHTML);
+  //   }
+  
+  //   var count = function(start, value, id) {
+  //     var localStart = start;
+  //     setInterval(function() {
+  //       if (localStart < value) {
+  //         localStart++;
+  //         counters[id].innerHTML = localStart;
+  //       }
+  //     }, 1);
+  //   }
+  
+  //   for (j = 0; j < countersQuantity; j++) {
+  //     count(0, counter[j], j);
+  //   }
+  // });
