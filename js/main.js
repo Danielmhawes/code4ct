017e0ea72dcd5285c545a8af1f73f444dd6aea9a
Danielmhawes <danielmhawes002@gmail.com>
2019-01-28 16:19:37 +0200

    editing with my checklist

--- js/main.js
+++ js/main.js
@@ -73,3 +73,25 @@ $(window).scroll(function() {
   //     count(0, counter[j], j);
   //   }
   // });
+
+  // progress bar
+
+  // progressbar.js@1.0.0 version is used
+// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+
+// back to top button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
